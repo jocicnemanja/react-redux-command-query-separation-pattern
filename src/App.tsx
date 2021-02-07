0f46e267252733aa3a0e694d2c0d8f5d4f916cab
Nemanja Jocic <nemanja.jocic.dev@gmail.com>
2021-02-07 19:09:23 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Page from "./pages/Page/Page";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>Neki random tekst</h1>
-      <h2>Neki random tekst</h2>
-      <h3>Neki random tekst</h3>
-      <p>Neki random tekst</p>
+      <Page />
     </div>
   );
 }
--- src/pages/Page/Page.stories.tsx
+++ src/pages/Page/Page.stories.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
+import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from "@storybook/react/types-6-0";
 
-import { Page, PageProps } from './Page';
-import * as HeaderStories from '../../components/Header/Header.stories';
+import Page from "./Page";
+import * as HeaderStories from "../../components/Header/Header.stories";
 
 export default {
-  title: 'Example/Page',
+  title: "Example/Page",
   component: Page,
 } as Meta;
 
-const Template: Story<PageProps> = (args) => <Page {...args} />;
+const Template: Story = () => <Page />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  ...HeaderStories.LoggedIn.args,
-};
-
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {
-  ...HeaderStories.LoggedOut.args,
+export const Primary = Template.bind({});
+Primary.args = {
+  ...HeaderStories.Primary.args,
 };
